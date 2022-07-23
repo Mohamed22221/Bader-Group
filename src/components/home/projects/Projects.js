1b2fc21eab0aa194e4bbc8d2308c7350babfc0a8
Mohamed2221 <87386058+mohamedhamdy12@users.noreply.github.com>
2022-07-23 16:53:16 +0200

    performance

--- src/components/home/projects/Projects.js
+++ src/components/home/projects/Projects.js
@@ -1,4 +1,4 @@
-import React, {  useState ,memo } from 'react'
+import React, {  useState ,memo, useMemo } from 'react'
 import Buttons from './Buttons'
 import { Container } from '@mui/material'
 import "./Projects.scss"
@@ -8,16 +8,16 @@ import data from "./AllData"
 const Projects = () => {
   const [menueItem , setMenueItem] = useState(data)
 
-  const Filter =  (button) =>{
+  const Filter = useMemo(()=> (button) =>{
     if(button === "All work"){
       setMenueItem(data)
       return;
     }
     const FilterdData = data.filter(item => item.type == button)
     setMenueItem(FilterdData )
-    
+    return menueItem
    
-  }
+  })
 
   return (
     <div className='projects glopal-margin'>
