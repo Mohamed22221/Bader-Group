869c3d9d89ea93f00fa3a15b9ae529d203069447
Mohamed2221 <87386058+mohamedhamdy12@users.noreply.github.com>
2022-09-24 11:54:49 +0200

    done photo view

--- src/components/home/projects/Menue.js
+++ src/components/home/projects/Menue.js
@@ -1,11 +1,22 @@
 import React ,{useEffect,useState} from 'react'
 import Carousel from "react-elastic-carousel";
-import axios from "axios";
+import { CgClose } from 'react-icons/cg';
+
   //get data api
 const Menue = ({categories}) => {
+  const [myPhoto , setMyPhoto] = useState(null)
+  const [togglePhoto , setTogglePhoto] = useState(true)
 
+const openPhoto = (img) => {
+  setMyPhoto(img)
+  setTogglePhoto(false)
+}
   return (
     <div className='menu'>
+      <div className={togglePhoto ?"model " :"model open" }>
+        <img src={myPhoto} />
+        <CgClose onClick={() =>setTogglePhoto(true)} />
+      </div>
      {categories.map(item =>{
       return (
        <div className='menu-item' key={item.id} >
@@ -13,15 +24,13 @@ const Menue = ({categories}) => {
           {item.images.map((img ,index )=>{
             return (
               <div key={index} >
-                <img src={img} />
+                <img src={img} onClick={() => openPhoto(img)} />
               </div>
             )
   
           })}
           </Carousel>
-          
         </div>
-      
       )
      })
      }
